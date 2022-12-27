dd03fd7ae6b26d8f9a05c637ea79d4fdb2439de9
Karan Mahato <44kyto@gmail.com>
2022-12-27 21:55:36 +0545

    Sidebar updates

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,6 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('charts') }}"><i class="la la-home nav-icon"></i> Charts</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('charts') }}"><i class="las la-chart-bar"></i> Charts</a></li>
 
 <!-- Users, Roles, Permissions -->
 <li class="nav-item nav-dropdown">
@@ -11,6 +11,6 @@
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
     </ul>
 </li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('blood-pack') }}"><i class="nav-icon la la-th-list"></i> Blood packs</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('donor') }}"><i class="nav-icon la la-th-list"></i> Donors</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('event') }}"><i class="nav-icon la la-th-list"></i> Events</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('blood-pack') }}"><i class="nav-icon la la-tint"></i> Blood packs</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('donor') }}"><i class="nav-icon las la-hand-holding-heart"></i> Donors</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('event') }}"><i class="nav-icon la la-bell"></i> Events</a></li>
