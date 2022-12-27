8d6407ea6618c11cf4cdb5472e2ee59991504cc3
Suprav Kandel <dynosups@outlook.com>
2022-12-27 21:47:40 +0545

    Bar chart for blood group and pie chart for filter by age range

--- app/Http/Controllers/Admin/ChartController.php
+++ app/Http/Controllers/Admin/ChartController.php
@@ -2,32 +2,50 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Charts\DonorChart;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+use App\Models\Donor;
 
 class ChartController extends Controller
 {
     public function index(Request $request)
     {
-        $chart1_options = [
-            'chart_title' => 'Donors by Blood Group',
-            'report_type' => 'group_by_string',
-            'model' => 'App\Models\Donor',
-            'group_by_field' => 'blood_group',
-            'chart_type' => 'bar',
-        ];
-        $chart1 = new LaravelChart($chart1_options);
+        $bg_data = Donor::pluck('blood_group')->countBy();
+        $donorsByBGChart = new DonorChart;
+        $donorsByBGChart->labels($bg_data->keys());
+        $donorsByBGChart->dataset('Donors by Blood Group', 'bar', $bg_data->values());
 
-        $chart2_options = [
-            'chart_title' => 'Donors by Age Range',
-            'report_type' => 'group_by_string',
-            'model' => 'App\Models\Donor',
-            'group_by_field' => 'age',
-            'chart_type' => 'pie',
-        ];
-        $chart2 = new LaravelChart($chart2_options);
-        
-        return view('admin.chart', compact('chart1', 'chart2'));
+        $age_data = Donor::pluck('age')->groupBy(function ($item, $key) {
+            if ($item < 15) {
+                return '0-15';
+            } elseif ($item < 20) {
+                return '15-20';
+            } elseif ($item < 25) {
+                return '20-25';
+            } elseif ($item < 30) {
+                return '25-30';
+            } elseif ($item < 35) {
+                return '30-35';
+            } elseif ($item < 40) {
+                return '35-40';
+            } elseif ($item < 45) {
+                return '40-45';
+            } elseif ($item < 50) {
+                return '45-50';
+            } else {
+                return '50+';
+            }
+        })->map(function ($group) {
+            return $group->count();
+        });
+        $donorsByAgeChart = new DonorChart;
+        $donorsByAgeChart->labels($age_data->keys());
+        $donorsByAgeChart->dataset('Donors by Blood Group', 'pie', $age_data->values());
+
+        return view('admin.chart', [
+            'donorsByBGChart' => $donorsByBGChart,
+            'donorsByAgeChart' => $donorsByAgeChart
+        ]);
     }
 }
