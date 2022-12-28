78719fee91798bb82613e162f0ebb0ae86cfb401
Suprav Kandel <dynosups@outlook.com>
2022-12-28 13:16:57 +0545

    Add graphs to the charts page

--- app/Http/Controllers/Admin/ChartController.php
+++ app/Http/Controllers/Admin/ChartController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Carbon\Carbon;
+use App\Models\Donor;
 use App\Charts\DonorChart;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Donor;
 
 class ChartController extends Controller
 {
@@ -85,9 +86,155 @@ class ChartController extends Controller
                 'rgb(0, 100, 0)',
             ]);
 
+        $bg_bp_data = \App\Models\BloodPack::with('donor')
+            ->where('is_sold', false)
+            ->get()
+            ->pluck('donor.blood_group')->countBy();
+        $bpByBGChart = new DonorChart;
+        // $bpByBGChart->minimalist(true);
+        $bpByBGChart->labels($bg_bp_data->keys());
+        $bpByBGChart->dataset('Blood Pack by Blood Group', 'pie', $bg_bp_data->values())
+            ->color([
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 206, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)',
+                'rgb(232, 67, 147)',
+                'rgb(255, 159, 64)',
+            ])
+            ->backgroundColor([
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 206, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)',
+                'rgb(232, 67, 147)',
+                'rgb(255, 159, 64)',
+            ]);
+
+        $bp_units_data = \App\Models\BloodPack::with('donor')
+            ->where('is_sold', false)
+            ->get()
+            ->groupBy('donor.blood_group')
+            ->map(fn ($gr) => $gr->sum('unit'));
+        $bpByBGUnitChart = new DonorChart;
+        // $bpByBGUnitChart->minimalist(true);
+        $bpByBGUnitChart->labels($bp_units_data->keys());
+        $bpByBGUnitChart->dataset('Blood Pack by Blood Group', 'bar', $bp_units_data->values())
+            ->color([
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 206, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)',
+                'rgb(232, 67, 147)',
+                'rgb(255, 159, 64)',
+            ])
+            ->backgroundColor([
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 206, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)',
+                'rgb(232, 67, 147)',
+                'rgb(255, 159, 64)',
+            ]);
+
+
+        $bp_types_data = \App\Models\BloodPack::pluck('blood_type')->countBy();
+        $bpByTypeChart = new DonorChart;
+        // $bpByTypeChart->minimalist(true);
+        $bpByTypeChart->labels($bp_types_data->keys());
+        $bpByTypeChart->dataset('Blood Pack by Blood Type', 'bar', $bp_types_data->values())
+            ->color([
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 206, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)',
+                'rgb(232, 67, 147)',
+                'rgb(255, 159, 64)',
+                'rgb(129, 199, 132)',
+                'rgb(255, 105, 180)',
+            ])
+            ->backgroundColor([
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 206, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)',
+                'rgb(232, 67, 147)',
+                'rgb(255, 159, 64)',
+                'rgb(129, 199, 132)',
+                'rgb(255, 105, 180)',
+            ]);
+
+        $bp_exp_data = \App\Models\BloodPack::select('expiry_at')
+            ->where('expiry_at', '>=', Carbon::now()->startOfMonth())
+            ->get()
+            ->groupBy(function ($record) {
+                return Carbon::parse($record->expiry_at)->format('F');
+            })->map(function ($group) {
+                return $group->count();
+            });
+        $bp_arr_data = \App\Models\BloodPack::select('expiry_at')
+            ->where('arrived_at', '>=', Carbon::now()->startOfMonth())
+            ->get()
+            ->groupBy(function ($record) {
+                return Carbon::parse($record->arrived_at)->format('F');
+            })->map(function ($group) {
+                return $group->count();
+            });
+        $bpByArrExpDateChart = new DonorChart;
+        // $bpByArrExpDateChart->minimalist(true);
+        $bpByArrExpDateChart->labels($bp_exp_data->keys());
+        $bpByArrExpDateChart->dataset('Blood Group Expiry', 'line', $bp_exp_data->values())
+            ->backgroundColor('rgba(255, 99, 132, 0.6)')
+            ->color('rgb(255, 99, 132)');
+        $bpByArrExpDateChart->dataset('Blood Group Arrival', 'line', $bp_arr_data->values())
+            ->backgroundColor('rgba(54, 162, 235, 0.6)')
+            ->color('rgb(54, 162, 235)');
+
+
+        $event_data = \App\Models\Event::select('event_at')
+            ->where('event_at', '>=', Carbon::now()->startOfMonth())
+            ->get()
+            ->groupBy(function ($record) {
+                return Carbon::parse($record->event_at)->format('F');
+            })->map(function ($group) {
+                return $group->count();
+            });
+        $eventDateChart = new DonorChart;
+        // $eventDateChart->minimalist(true);
+        $eventDateChart->labels($event_data->keys());
+        $eventDateChart->dataset('Events', 'bar', $event_data->values())
+            ->backgroundColor([
+                'rgb(255, 99, 71)', 'rgb(54, 162, 235)', 'rgb(255, 206, 86)', 'rgb(75, 192, 192)', 'rgb(153, 102, 255)',
+                'rgb(255, 159, 64)', 'rgb(233, 30, 99)', 'rgb(205, 92, 92)', 'rgb(103, 58, 183)', 'rgb(0, 150, 136)',
+                'rgb(139, 0, 139)', 'rgb(0, 188, 212)'
+            ])
+            ->color([
+                'rgb(255, 99, 71)', 'rgb(54, 162, 235)', 'rgb(255, 206, 86)', 'rgb(75, 192, 192)', 'rgb(153, 102, 255)',
+                'rgb(255, 159, 64)', 'rgb(233, 30, 99)', 'rgb(205, 92, 92)', 'rgb(103, 58, 183)', 'rgb(0, 150, 136)',
+                'rgb(139, 0, 139)', 'rgb(0, 188, 212)'
+            ]);
+
+
         return view('admin.chart', [
             'donorsByBGChart' => $donorsByBGChart,
-            'donorsByAgeChart' => $donorsByAgeChart
+            'donorsByAgeChart' => $donorsByAgeChart,
+            'bpByBGChart' => $bpByBGChart,
+            'bpByBGUnitChart' => $bpByBGUnitChart,
+            'bpByTypeChart' => $bpByTypeChart,
+            'bpByArrExpDateChart' => $bpByArrExpDateChart,
+            'eventDateChart' => $eventDateChart,
         ]);
     }
 }
--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,6 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('charts') }}"><i class="las la-chart-bar"></i> Charts</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('charts') }}"><i class="la la-chart-bar nav-icon"></i> Charts</a></li>
 
 <!-- Users, Roles, Permissions -->
 <li class="nav-item nav-dropdown">
